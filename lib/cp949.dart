378da764be9da9502ce9cd9fe9e97519baa18cef
sangik <sang@sellmate.co.kr>
2023-02-27 09:48:23 +0900

    null safe 하도록 수정

--- lib/cp949.dart
+++ lib/cp949.dart
@@ -1,19 +1,19 @@
 import 'src/code_map.dart' show cp949ToUnicodeCodeMap, unicodeToCp949CodeMap;
 
 List<int> toUnicodes(final List<int> codeUnits) {
-  final unicodes = List<int>(codeUnits.length);
+  final List<int> unicodes = List.filled(codeUnits.length, 0);
   var j = 0;
   for (var i = 0; i < codeUnits.length; j++) {
     // When 1 byte becomes 1 cp949 code
     if (0x00 <= codeUnits[i] && codeUnits[i] <= 0x7F) {
-      unicodes[j] = cp949ToUnicodeCodeMap[codeUnits[i]];
+      unicodes[j] = cp949ToUnicodeCodeMap[codeUnits[i]] ?? 0;
       i = i + 1;
     } else {
       // When 2 bytes become 1 cp949 code
       final cp949Code = (codeUnits[i] << 8) + codeUnits[i + 1];
       i = i + 2;
       if (0x8141 <= cp949Code && cp949Code <= 0xFDFE) {
-        unicodes[j] = cp949ToUnicodeCodeMap[cp949Code];
+        unicodes[j] = cp949ToUnicodeCodeMap[cp949Code] ?? 0;
       } else {
         throw FormatException(
             'Invalid code unit of CP949. It has to be (>=0x00 <=0x7F) || (>=0x8141 <=0xFDFE).');
@@ -24,6 +24,13 @@ List<int> toUnicodes(final List<int> codeUnits) {
 }
 
 String decode(final List<int> codeUnits) {
+
+  final unicodeData = toUnicodes(codeUnits);
+
+  if (unicodeData.contains(null)) {
+    throw NullThrownError();
+  }
+
   return String.fromCharCodes(toUnicodes(codeUnits));
 }
 
@@ -34,12 +41,18 @@ String decodeString(final String brokenString) {
 /// Dart does not support non-unicode encoding.
 /// Thus, the return value has to be raw byte array of CP949.
 List<int> encode(final String str) {
-  final cp949codeUnits = List<int>(str.codeUnits.length * 2);
+  final List<int> cp949codeUnits = List.filled(str.codeUnits.length * 2, 0);
   var i = 0;
   for (final unicode in str.codeUnits) {
     final cp949Code = unicodeToCp949CodeMap[unicode];
-    final firstByte = cp949Code >> 8;
-    final secondByte = cp949Code - (firstByte << 8);
+    var firstByte = 0;
+    var secondByte = 0;
+
+    if (cp949Code != null) {
+      firstByte = cp949Code >> 8;
+      secondByte = cp949Code - (firstByte << 8);
+    }
+
     if (firstByte != 0) {
       cp949codeUnits[i] = firstByte;
       i = i + 1;
@@ -51,5 +64,11 @@ List<int> encode(final String str) {
 }
 
 String encodeToString(final String str) {
+  final encodeData = encode(str);
+
+  if (encodeData.contains(null)) {
+    throw NullThrownError();
+  }
+
   return String.fromCharCodes(encode(str));
 }
